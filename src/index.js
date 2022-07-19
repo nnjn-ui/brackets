7459464fd5cdee58fdeff8986030d14aeb7184ae
Daniel Bolshenko <danil.bygay@gmail.com>
2022-07-19 09:16:28 +0200

    init: my work index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openingBrackets = [];
+  let closingBrackets = [];
+  let sameBrackets = [];
+  let utilityArray = [];
+
+bracketsConfig.forEach(function(element) {
+    if (element[0] === element[1]) {
+        sameBrackets.push(element[0]);
+    }
+    else {
+        openingBrackets.push(element[0]);
+        closingBrackets.push(element[1]);
+    }
+})
+
+for (let i = 0; i < str.length; i++) {
+    if (openingBrackets.includes(str[i])) {
+        utilityArray.push(str[i]);
+    }
+    else if (closingBrackets.includes(str[i])) {
+        if(closingBrackets.indexOf(str[i]) === openingBrackets.indexOf(utilityArray[utilityArray.length - 1])) {
+            utilityArray.pop();
+        }
+        else {
+            utilityArray.push(str[i]);
+        }
+    }
+    else if (sameBrackets.includes(str[i])) {
+        if (str[i] === utilityArray[utilityArray.length - 1]) {
+            utilityArray.pop();
+        }
+        else {
+            utilityArray.push(str[i]);
+        }
+    }
+}
+
+return utilityArray.length === 0;
 }
